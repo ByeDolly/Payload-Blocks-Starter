e3c3ece8badf7e2973ede90a1041db45f23ee5a6
Jeremy Englert <jeremy@showzone.io>
2024-11-10 17:56:40 -0700

    Prepare for NPX package

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,78 +1,63 @@
 import { buildConfig } from "payload";
-
 import sharp from "sharp";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
-// import { postgresAdapter } from "@payloadcms/db-postgres";
-import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { seoPlugin } from "@payloadcms/plugin-seo";
 
 import { Pages } from "./collections/Pages";
 import { Posts } from "./collections/Posts";
-
 import { Media } from "./collections/Media";
 import Users from "./collections/Users";
 
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+
 export default buildConfig({
-	// Define and configure your collections in this array
-	collections: [Pages, Posts, Media, Users],
+  collections: [Pages, Posts, Media, Users],
 
-	plugins: [
-		seoPlugin({
-			collections: [],
-			uploadsCollection: "media",
-            tabbedUI: true,
-			generateTitle: ({ doc }) => `Website.com — ${doc.title}`,
-			generateDescription: ({ doc }) => doc.excerpt,
-		}),
-	],
+  plugins: [
+    seoPlugin({
+      collections: [],
+      uploadsCollection: "media",
+      tabbedUI: true,
+      generateTitle: ({ doc }) => `${doc.title}`,
+      generateDescription: ({ doc }) => doc.excerpt,
+    }),
+  ],
 
-	// If you'd like to use Rich Text, pass your editor here
-	editor: lexicalEditor(),
+  editor: lexicalEditor(),
 
-	// Whichever Database Adapter you're using should go here
-	//   db: postgresAdapter({
-	//     pool: {
-	//       connectionString: process.env.DATABASE_URL,
-	//     },
-	//   }),
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || "",
+  }),
 
-	db: mongooseAdapter({
-		url: process.env.DATABASE_URL || "",
-	}),
+  sharp,
 
-	// If you want to resize images, crop, set focal point, etc.
-	// This is optional - if you don't need to do these things,
-	sharp,
+  admin: {
+    avatar: "default",
+    livePreview: {
+      breakpoints: [
+        {
+          label: "Mobile",
+          name: "mobile",
+          width: 375,
+          height: 667,
+        },
+        {
+          label: "Tablet",
+          name: "tablet",
+          width: 768,
+          height: 1024,
+        },
+        {
+          label: "Desktop",
+          name: "desktop",
+          width: 1440,
+          height: 900,
+        },
+      ],
+    },
+  },
 
-	// Adjust admin dashboard
-	admin: {
-		avatar: "default",
-		livePreview: {
-			breakpoints: [
-				{
-					label: "Mobile",
-					name: "mobile",
-					width: 375,
-					height: 667,
-				},
-				{
-					label: "Tablet",
-					name: "tablet",
-					width: 768,
-					height: 1024,
-				},
-				{
-					label: "Desktop",
-					name: "desktop",
-					width: 1440,
-					height: 900,
-				},
-			],
-		},
-	},
+  globals: [],
 
-	globals: [],
-
-	// Your Payload secret - should be a complex and secure string, unguessable
-	secret: process.env.PAYLOAD_SECRET || "",
+  secret: process.env.PAYLOAD_SECRET || "",
 });
